5e3f20cc7b9a80bba53eaf990b1769265c4a2b89
Karol Trombala <ktrombala@gmail.com>
2023-07-07 14:41:23 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+let timerId = null;
+
+startBtn.addEventListener('click', () => {
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+    timerId = setInterval (() => {
+        document.body.style.background = getRandomHexColor()
+    }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+   clearInterval(timerId);
+});
+
 
